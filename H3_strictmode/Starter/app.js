05382b96387c58daf96f535d33455a5ee83baa27
Jessica <jdhpabst@gmail.com>
2017-07-10 16:00:48 -0600

    finished up to lecture 69

---
+++ H3_strictmode/Starter/app.js
@@ -0,0 +1,23 @@
+// STRICT MODE //
+
+function logNewPerson(){
+    "use strict"; //only works for the function, not rest of the file
+
+    var person2;
+    // let's say we misspell our 'person' variable //
+    persom2 = {};
+    console.log(persom2);
+    // after strict mode is typed above, it will not let you call a variable unless it has been set to a var.
+
+}
+
+var person;
+// let's say we misspell our 'person' variable //
+persom = {};
+console.log(persom);
+// strict only applies to function above, so this will pass and not throw errors //
+logNewPerson();
+
+
+// Why not use strict all the time? -- it's an extra feature, can't rely on 100%, but can still be useful. // mozilla developer website has a description at : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode //
+
