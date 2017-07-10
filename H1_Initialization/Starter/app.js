0e83db55239766bc0729a4c0be9fa5acbfc99154
Jessica <jdhpabst@gmail.com>
2017-07-10 15:47:40 -0600

    finished up to lesson 66

---
+++ H1_Initialization/Starter/app.js
@@ -0,0 +1,23 @@
+var people = [
+    {
+        firstname: 'Jessica',
+        lastname: "Pabst",
+        addresses: [
+            '111 Main St',
+            '222 Third St'
+        ]
+    },
+    {
+        firstname: 'John',
+        lastname: 'Doe',
+        addresses: [
+            '333 Main St',
+            '444 Thirds St'
+        ],
+        greet: function(){
+            return 'Hello!'
+        }
+    }
+]
+
+console.log(people)
