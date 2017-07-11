5be777b4a2945d77d31292205b48f2ff5a8c9c9f
Jessica <jdhpabst@gmail.com>
2017-07-11 11:06:01 -0600

    added commenting to the test code

--- J7_LetsUseOurFramework/Starter/app.js
+++ J7_LetsUseOurFramework/Starter/app.js
@@ -1,13 +1,18 @@
-var g = G$('John', 'Doe');
 
+// gets a new object (the architecture allows us to not have to use the 'new' keyword here)
+var g = G$('John', 'Doe');
+// use our chainable methods
 g.greet().setLang('es').greet(true).log();
 
+// let's use our object on the click of the login button
 $('#login').click(function(){
 
+
+    // create a new 'Greetr' objects (let's pretend we know teh name from the login)
     var loginGrtr = G$('Jane', 'Doe');
-
+    // hide the login on the screen
     $('#logindiv').hide();
-
+    // fire off an HTML greeting, passing the '#greeting' as the selector and the chosen language, and log teh welcome as well
     loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
 
 
